41907d496985e9d7901bc4d487a235fe9fdd4d21
zorazky <oswaldo.vazquez@gmail.com>
2019-09-30 13:11:29 -0500

    finished

--- store.js
+++ store.js
@@ -6,7 +6,6 @@ if (document.readyState == 'loading') {
 
 function ready() {
     const removeCartItemButtons = document.getElementsByClassName('btn-danger');
-
     for (let i = 0; i < removeCartItemButtons.length; i++) {
         const button = removeCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
@@ -17,6 +16,24 @@ function ready() {
         let input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
+
+    const addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (let i = 0; i < addToCartButtons.length; i++) {
+        const button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    const cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+
+    }
+    updateCartTotal()
 }
 
 
@@ -33,6 +50,45 @@ function quantityChanged(event) {
         input.value = 1
     }
     updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    const button = event.target
+    const shopItem = button.parentElement.parentElement
+    const title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    const price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    const imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].imageSrc
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    const cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    const cartItems = document.getElementsByClassName('cart-items')[0]
+    const cartItemNames = cartItemNames.getElementsByClassName('cart-item-title')
+    for (let i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert(`This item is already added to the cart`)
+            return
+        }
+    }
+    
+    const cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column>${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+    `
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+
 }
 
 
